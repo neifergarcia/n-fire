344a878a3b4ba36ebf480ef2386702bcf157f0df
Neifer Garcia <neifergarcia@gmail.com>
2018-07-07 10:55:45 -0500

    remove geo-fire

--- src/main/java/com/ene/geo/Models.kt
+++
@@ -1,9 +0,0 @@
-package com.ene.geo
-
-import com.ene.core.Object
-
-data class GeoDataLocation(val latitude: Double = 0.0, val longitude: Double = 0.0)
-
-data class GeoData(
-  val location: GeoDataLocation = GeoDataLocation(), var hash: String = ""
-) : Object()
